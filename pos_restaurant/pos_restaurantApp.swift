b30ec39f1c8393ef60d4c657cd66352fb32a0728
May Yi Aung <may@aiems.ae>
2024-07-28 13:35:25 +0400

    Initial Commit

---
+++ pos_restaurant/pos_restaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  pos_restaurantApp.swift
+//  pos_restaurant
+//
+//  Created by May Yi on 28/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct pos_restaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
